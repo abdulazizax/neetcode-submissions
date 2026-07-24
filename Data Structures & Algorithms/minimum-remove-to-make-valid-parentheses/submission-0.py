c612f7c7cd1bc5a53f561be2a30fc6bb0a8cb62e
Abdulaziz Kholikulov <165239434+abdulazizax@users.noreply.github.com>
2026-07-24 16:05:54 +0500

    Add: minimum-remove-to-make-valid-parentheses - submission-0

---
+++ Data Structures & Algorithms/minimum-remove-to-make-valid-parentheses/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        s = list(s)
+        stack = []
+
+        for i, c in enumerate(s):
+            if c == '(':
+                stack.append(i)
+            elif c == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    s[i] = ""
+
+        # print("s ==>", s)
+        # print("stack ==> ", stack)
+
+        while stack:
+            s[stack.pop()] = ''
+
+        return ''.join(s)
