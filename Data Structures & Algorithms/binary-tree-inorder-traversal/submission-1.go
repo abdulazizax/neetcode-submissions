cf2fd0fa62a0490b5bfe18dc3693e646da1e54b2
Abdulaziz Kholikulov <165239434+abdulazizax@users.noreply.github.com>
2026-09-14 19:09:10 +0500

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.go
@@ -0,0 +1,19 @@
+func inorderTraversal(root *TreeNode) []int {
+    res := []int{}
+
+    var dfs func(root *TreeNode)
+
+    dfs = func(root *TreeNode) {
+        if root == nil {
+            return
+        }
+
+        dfs(root.Left)
+        res = append(res, root.Val)
+        dfs(root.Right)
+    }
+
+    dfs(root)
+
+    return res
+}
